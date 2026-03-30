778c632842037b8eed31fbff38b7f47c38b85aae
UsernameUdit <uditpanda794@gmail.com>
2026-03-31 00:50:14 +0530

    Shri Ganesh of Imagedawg

---
+++ main.py
@@ -0,0 +1,55 @@
+import subprocess as sb
+from pathlib import Path
+print("Welcome to Imagedawg your offline CLI image converter\n")
+
+def get_input_path():
+    c = input("Enter full path of the image\n")
+    file_path = Path(c)
+
+    if file_path.exists():
+        print(f"The path '{file_path}' exists.")
+    else:
+        print(f"The path '{file_path}' does not exist.")
+        exit()
+
+    return c
+
+def detect_format(path):
+    inp_format = [".png",".jpg",".webp"]
+    if path.endswith(".png"):
+        print("Input format .png detected")
+        return inp_format[0]
+    elif path.endswith(".jpg"):
+        print("Input format .jpg detected")
+        return inp_format[1]
+    elif path.endswith(".webp"):
+        print("Input format .webp detected")
+        return inp_format[2]
+    else:
+        print("The given format is invalid")
+        exit()
+
+def get_output_format():
+    out_format = [".png",".jpg",".webp"]
+    a = input("type .jpg/.webp/.png to convert into the given format")
+    if a==".jpg":
+            return out_format[1]
+    elif a==".png":
+        return out_format[0]
+    elif a==".webp":
+            return out_format[2]
+    else:
+        print("Invalid choice")
+        exit()
+
+
+def convert(path,gof):
+    command = ["ffmpeg", "-i",path, "img123"+gof]
+    sb.run(command,shell = False)
+
+path = get_input_path()
+inp_format = detect_format(path)
+out_format = get_output_format(inp_format)
+convert(path, out_format)
+
+
