2cf59e8a100f0dd0835108eb36de62f3376fcae2
UsernameUdit <uditpanda794@gmail.com>
2026-04-03 03:17:55 +0530

    looksmaxxing

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import subprocess as sb
+from pyfiglet import figlet_format
+import questionary as q
 from pathlib import Path
-from rich import print
+from rich.console import Console
+from rich.text import Text
 
-print("Welcome to [italic red]Imagedawg[/italic red] your offline CLI image converter\n")
+supported = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".webp",
+    ".avif"
+]
+
+console = Console()
+art = figlet_format("Imagedawg", font="slant")
+
+text = Text(art)
+text.stylize("bold rgb(220,0,0) on rgb(0,0,0)")
+console.print(text)
+console.print("Welcome to [italic red]Imagedawg[/italic red] your offline CLI image converter\n")
+console.print(f"Image format supported [sea_green2]'{supported}'[/sea_green2]")
 
 def get_input_path():
-    c = input("Enter full path of the image\n")
+    c = q.text("Enter full path of the image").ask()
     file_path = Path(c)
 
     if file_path.exists():
@@ -14,32 +32,19 @@ def get_input_path():
         print(f"The path '{file_path}' does not exist.")
         exit()
 
-    return c
+    return file_path
 
 def detect_format(path):
-    inp_format = [".png",".jpg",".webp"]
-    if path.endswith(".png"):
-        print("Input format .png detected")
-        return inp_format[0]
-    elif path.endswith(".jpg"):
-        print("Input format .jpg detected")
-        return inp_format[1]
-    elif path.endswith(".webp"):
-        print("Input format .webp detected")
-        return inp_format[2]
+    if path.suffix.lower() in supported:
+        print("The given format exists throwing the file for the next step")
     else:
         print("The given format is invalid")
         exit()
 
 def get_output_format():
-    out_format = [".png",".jpg",".webp"]
-    a = input("type .jpg/.webp/.png to convert into the given format\n")
-    if a==".jpg":
-            return out_format[1]
-    elif a==".png":
-        return out_format[0]
-    elif a==".webp":
-            return out_format[2]
+    a = input("type .jpg/.webp/.png or any other image format to convert into the given format\n")
+    if a in supported:
+        return a
     else:
         print("Invalid choice")
         exit()
@@ -49,7 +54,6 @@ def convert(path,gof):
     print("Violating your state secrets beeeep boooop beeeeep................")
     command = ["ffmpeg", "-i",path, "img123"+gof]
     sb.run(command,shell = False)
-    print("File cooked dawg")
 
 
 path = get_input_path()
